0db44215c4952a3bd0c3a23f9dd2bae7ff91a3e9
Alec Springel <alecspringel@gmail.com>
2020-11-24 19:00:33 -0800

    Created home page header and remove false Poppins font

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
+import Home from "./components/home/Home";
+
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <Home />
+    </div>
+  );
 }
 
 export default App;
--- src/components/home/Header.js
+++ src/components/home/Header.js
@@ -1,7 +1,47 @@
 import React from "react";
+import BackgroundImg from "../../assets/home-header.jpg";
+import styled from "styled-components";
+import LLLogo from "../../assets/ll-logo.svg";
 
 const Header = () => {
-  return <div></div>;
+  return (
+    <div>
+      <Background>
+        <Overlay className="flex-row just align">
+          <Logo src={LLLogo} />
+          <HugeH1>Software development for a better world.</HugeH1>
+        </Overlay>
+      </Background>
+    </div>
+  );
 };
 
 export default Header;
+
+const Background = styled.section`
+  position: relative;
+  width: 100%;
+  background: url(${BackgroundImg});
+  background-size: cover;
+  height: 100vh;
+`;
+
+const Overlay = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background: #00000050;
+`;
+
+const HugeH1 = styled.h1`
+  font-size: 55px;
+`;
+
+const Logo = styled.img`
+  position: absolute;
+  left: 30px;
+  top: 30px;
+  height: 43px;
+`;
